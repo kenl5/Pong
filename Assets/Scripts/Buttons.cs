4fb45dd593e8190d6434b26c791d498aa6ea9306
kenl5 <kenl5@uci.edu>
2019-05-31 16:36:13 -0700

    first commit

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons: MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("StartMenu", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
