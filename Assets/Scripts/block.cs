8a1cbeb4bb78a73271eb2c22029cb5772be30634
Kaifan <kaifanx@uci.edu>
2019-06-12 16:57:53 -0700

    no message

---
+++ Assets/Scripts/block.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class block : MonoBehaviour
+{
+    public float delay = 3.0f;
+
+    void Start()
+    {
+        Destroy(gameObject, delay);
+    }
+}
---
+++ Assets/Scripts/setupplayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class setupplayer : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer)
+        {
+            GetComponent<Player0>().enabled = true;
+        }
+    }
+
+}
